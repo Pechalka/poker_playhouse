c59e75a1d5f29d86d7089e106ba5877b8089eed0
Gaidar Sinyakou <magnusducatuslt@gmail.com>
2022-05-23 02:56:08 +0300

    feature/tokens: properly accept mr

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,16 +1,5 @@
 module.exports = {
   apps : [{
-<<<<<<< HEAD
-    name: 'pokerhouse',
-    script: './server/index.js',
-    env:{
-      JWT_SECRET:"test",
-      PORT:8000,
-      NODE_ENV:"production"
-    },
-  }],
-};
-=======
     name: 'test_db',
     test_user: 'qwerty',
     script: './server/index.js',
@@ -21,4 +10,3 @@ module.exports = {
     },
   }],
 };
->>>>>>> 040461fe5bd069a0a66071bcf4a370c06a890a1e
